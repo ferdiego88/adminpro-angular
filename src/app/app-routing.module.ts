ec6b8fbf632e04351efb71d206dda5c1ffd15393
Fernando Santillan <ferdiego88@gmail.com>
2021-03-20 23:51:11 -0500

    fsv-pathDashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Modulos
 import { PagesRoutingModule } from './pages/pages.routing';
-
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
 import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
 
 
 const routes: Routes = [
+  // path: '/dashboard' PagesRouting
+  // path: '/auth' AuthRouting
+  // path: '/medicos' MedicosRouting
+  // path: '/compras' ComprasRouting
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: '**', component: NotpagefoundComponent},
 ];
 
