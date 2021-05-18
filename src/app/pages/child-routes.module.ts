b17291e7998580cf469d5e213ec2aae66245c342
Fernando Santillan <ferdiego88@gmail.com>
2021-05-17 23:22:31 -0500

    Lazy load

---
+++ src/app/pages/child-routes.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { PerfilComponent } from './perfil/perfil.component';
+
+// Mantenimientos
+import { UsuarioComponent } from './mantenimientos/usuario/usuario.component';
+import { MedicoComponent } from './mantenimientos/medico/medico.component';
+import { HospitalComponent } from './mantenimientos/hospital/hospital.component';
+import { MantenimientomedicoComponent } from './mantenimientos/medico/mantenimientomedico/mantenimientomedico.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { AdminGuard } from '../guards/admin.guard';
+
+const  chilRoutes: Routes = [
+  {path: '', component: DashboardComponent, data: {titulo: 'Dashboard'} },
+  {path: 'progress', component: ProgressComponent, data: {titulo: 'ProgressBar'}},
+  {path: 'grafica1', component: Grafica1Component, data: {titulo: 'Gráfica 1'}},
+  {path: 'account-settings', component: AccountSettingsComponent, data: {titulo: 'Ajustes de Tema'}},
+  {path: 'buscar/:termino', component: BusquedaComponent, data: {titulo: 'Búsquedas'}},
+  {path: 'promesas', component: PromesasComponent, data: {titulo: 'Promesas'} },
+  {path: 'perfil', component: PerfilComponent, data: {titulo: 'Perfil del Usuario'} },
+  {path: 'rxjs', component: RxjsComponent, data: {titulo: 'RXJS'}},
+  // Mantenimientos
+  {path: 'medicos', component: MedicoComponent, data: {titulo: 'Mantenimiento de Medicos'}},
+  {path: 'hospitales', component: HospitalComponent, data: {titulo: 'Mantenimiento de Hospitales'}},
+  {path: 'medico/:id', component: MantenimientomedicoComponent, data: {titulo: 'Mantenimiento de Medicos'}},
+  // {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+
+  // Rutas de Admin
+  {path: 'usuarios', canActivate: [AdminGuard], component: UsuarioComponent, data: {titulo: 'Mantenimiento de Usuarios'}},
+]
+
+@NgModule({
+  imports: [ RouterModule.forChild(chilRoutes)],
+  exports: [RouterModule]
+})
+export class ChildRoutesModule { }
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -3,19 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AuthGuard } from '../guards/auth.guard';
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromesasComponent } from './promesas/promesas.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { PerfilComponent } from './perfil/perfil.component';
-import { UsuarioComponent } from './mantenimientos/usuario/usuario.component';
-import { MedicoComponent } from './mantenimientos/medico/medico.component';
-import { HospitalComponent } from './mantenimientos/hospital/hospital.component';
-import { MantenimientomedicoComponent } from './mantenimientos/medico/mantenimientomedico/mantenimientomedico.component';
-import { BusquedaComponent } from './busqueda/busqueda.component';
-import { AdminGuard } from '../guards/admin.guard';
+
 
 
 
@@ -23,25 +11,8 @@ const routes: Routes = [
   {path: 'dashboard',
   component: PagesComponent,
   canActivate: [AuthGuard],
-  children:
-  [
-    {path: '', component: DashboardComponent, data: {titulo: 'Dashboard'} },
-    {path: 'progress', component: ProgressComponent, data: {titulo: 'ProgressBar'}},
-    {path: 'grafica1', component: Grafica1Component, data: {titulo: 'Gráfica 1'}},
-    {path: 'account-settings', component: AccountSettingsComponent, data: {titulo: 'Ajustes de Tema'}},
-    {path: 'buscar/:termino', component: BusquedaComponent, data: {titulo: 'Búsquedas'}},
-    {path: 'promesas', component: PromesasComponent, data: {titulo: 'Promesas'} },
-    {path: 'perfil', component: PerfilComponent, data: {titulo: 'Perfil del Usuario'} },
-    {path: 'rxjs', component: RxjsComponent, data: {titulo: 'RXJS'}},
-    // Mantenimientos
-    {path: 'medicos', component: MedicoComponent, data: {titulo: 'Mantenimiento de Medicos'}},
-    {path: 'hospitales', component: HospitalComponent, data: {titulo: 'Mantenimiento de Hospitales'}},
-    {path: 'medico/:id', component: MantenimientomedicoComponent, data: {titulo: 'Mantenimiento de Medicos'}},
-    // {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-
-    // Rutas de Admin
-    {path: 'usuarios', canActivate: [AdminGuard], component: UsuarioComponent, data: {titulo: 'Mantenimiento de Usuarios'}},
-  ]
+  canLoad: [AuthGuard],
+  loadChildren: () => import('./child-routes.module').then(m => m.ChildRoutesModule)
 },
 ];
 
